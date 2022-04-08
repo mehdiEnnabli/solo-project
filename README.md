ca17659f822586685535af3391fd4ce4e2d44611
mehdiEnnabli <mehdi.ennabli01@gmail.com>
2022-04-08 18:02:08 +0100

    done and tested

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # solo-project
 teacher dashboard
+if you want to try it please set this array in your localStorage key "teachers"
+[{"name":"ali","pwd":"ali12345"},{"name":"houssem","pwd":"houssem12345"},{"name":"hannen","pwd":"hannen12345"},{"name":"houda","pwd":"houda12345"}]
--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function Student() {
         lastName: "",
         email: "",
         age: "",
-        skills: [],
+        skills: { html: false, css: false, js: false, jquery: false },
         addStudent: addStudent,
         addSkill: addSkill,
-        modifStudent: modifStudent
+        modifStudent: modifStudent,
     }
 }
 var addStudent = function (firstName, lastName, email, age) {
@@ -17,14 +17,58 @@ var addStudent = function (firstName, lastName, email, age) {
     this.email = email
     this.age = age
 }
-var addSkill = function (skill) {
-    this.skills.push(skill)
+var addSkill = function (key, value) {
+    this.skills[key] = value
 }
 var modifStudent = function (key, value) {
-    this[key] = value
+
+    [key] = value
 }
+function printskills(object, i) {
+    var result = ''
+    for (var key in object) {
+        var check = ''
+        if (object[key]) {
+            check = "checked"
+        }
+        result += `<div class="form-check form-check-inline">
+            <input ${check} style="margin-left:10px" class="form-check-input" type="checkbox" value="${key}" id="${key}${i}"><label class="form-check-label" for="${key}">${key.toUpperCase()}</label>
+        </div>`
+    }
+    return result
+}
+function skilladd(n) {
+    var temp = JSON.parse(localStorage.getItem("students"))
+    for (var key in temp[n].skills) {
+        console.log($(`#${key}${n}`).is(':checked'));
 
+        if ($(`#${key}${n}`).is(':checked')) {
+            console.log("hello");
+            temp[n].skills[key] = true
+        }
+        else {
 
+            temp[n].skills[key] = false
+        }
+    }
+    localStorage.setItem("students", JSON.stringify(temp))
+}
+function test(student,cond1,cond2,cond3,cond4){
+    var result=true
+    if(cond1){
+        result=result&&student.html
+    }
+    if(cond2){
+        result=result&&student.css
+    }
+    if(cond3){
+        result=result&&student.js
+    }
+    if(cond4){
+        result=result&&student.jquery
+    }
+    return result
+}
 
 $(document).ready(function () {
 
@@ -77,37 +121,53 @@ $(document).ready(function () {
         location.reload();
     })
     //add student to a table from array of student using each ES6 syntax arrow function
-    var students = JSON.parse(localStorage.getItem("students"))   
-    students.forEach((element,i) => {
+    var students = JSON.parse(localStorage.getItem("students"))
+    students.forEach((element, i) => {
         $("tbody").append(` <tr><th scope="row">${i + 1}</th>
         <td colspan="1">${element.firstName}</td>
         <td colspan="1">${element.lastName}</td>
         <td colspan="1">${element.age}</td>
         <td colspan="1">${element.email}</td>
-        <td colspan="2">${element.skills}</td>
+        <td colspan="1"><button onclick="skilladd(${i})" id="skill${i}" type="button" class="btn btn-primary">Add Skill</button>
+        
+        ${printskills(element.skills, i)}
+        
+        </td>
         </tr>`)
     });
     // search button
     $("#searchStudent").click(function (e) {
-        var students = JSON.parse(localStorage.getItem("students"))   
-        var newTable=""
-        filterdArray=students.filter(student => student.lastName.includes($('#search-input').val()) || student.firstName.includes($('#search-input').val()))
-        filterdArray.forEach((element,i) => {
-            newTable+=(` <tr><th scope="row">${i + 1}</th>
+        var students = JSON.parse(localStorage.getItem("students"))
+        var newTable = ""
+        filterdArray = students.filter(student => 
+            ( student.lastName.includes($('#search-input').val())
+            || student.firstName.includes($('#search-input').val()))
+             && ((student.skills.html === $("#searchHtml").is(':checked'))
+             || (student.skills.css === $("#searchCss").is(':checked'))
+             || (student.skills.js === $("#searchJs").is(':checked'))
+             || (student.skills.jquery === $("#searchJquery").is(':checked')))
+            )
+            
+        filterdArray.forEach((element, i) => {
+            newTable += (` <tr><th scope="row">${i + 1}</th>
             <td colspan="1">${element.firstName}</td>
             <td colspan="1">${element.lastName}</td>
             <td colspan="1">${element.age}</td>
             <td colspan="1">${element.email}</td>
-            <td colspan="2">${element.skills}</td>
+            <td colspan="1"><button onclick="skilladd(${i})" id="skill${i}" type="button" class="btn btn-primary">Add Skill</button>
+            
+            ${printskills(element.skills, i)}
+
+            </td>
             </tr>`)
         });
         $("tbody").replaceWith(newTable)
-        
-        
+
+
     })
     // display students stored in the delet select
-    var students = JSON.parse(localStorage.getItem("students"))   
-    students.forEach((element,i) => {
+    var students = JSON.parse(localStorage.getItem("students"))
+    students.forEach((element, i) => {
         $("#toDelete").append(`<option value="${i}">${element.firstName} ${element.lastName}</option>`)
 
     });
@@ -117,10 +177,10 @@ $(document).ready(function () {
             $("#toDelete").replaceWith("<p class='error'>no student to delete</p>")
         }
         else
-        var temp = JSON.parse(localStorage.getItem("students"))
-            temp.splice($("#toDelete").val(),1)
-            localStorage.setItem("students", JSON.stringify(temp))
-            location.reload();
+            var temp = JSON.parse(localStorage.getItem("students"))
+        temp.splice($("#toDelete").val(), 1)
+        localStorage.setItem("students", JSON.stringify(temp))
+        location.reload();
     })
 
 
